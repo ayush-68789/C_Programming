9650aa5997234a8de2f445bc1fbae371f0b78b58
ayush-68789 <ayush.chaurasia_cs24@gla.ac.in>
2024-12-14 19:04:00 +0530

    Update print indexx of char in string.c

--- print indexx of char in string.c
+++ print indexx of char in string.c
@@ -5,12 +5,6 @@ int main()
     char str[100] ; 
     fgets(str,100,stdin) ;
     char ch ; 
-    scanf("%c",&ch) ;
-    if (str[strlen(str)-1] == '\n')
-    {
-        str[strlen(str)-1] == '\0' ; 
-    }
-    
     for (int i = 0 ; str[i] != '\0' ;i++)
     {
         if (ch==str[i])
